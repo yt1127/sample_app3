7dcc0684cf624bb1489939642bdcf8644f3a5533
yoshiyuki <karaageninnikudashi@gmail.com>
2019-10-04 16:21:26 +0900

    lmplement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
 
-  get  '/help',    to: 'static_pages#help'
+  get      '/help',    to: 'static_pages#help'
 
-  get  '/about',   to: 'static_pages#about'
+  get     '/about',    to: 'static_pages#about'
 
-  get  '/contact', to: 'static_pages#contact'
+  get     '/contact',  to: 'static_pages#contact'
 
-  get  '/signup',  to: 'users#new'
+  get     '/signup',   to: 'users#new'
 
-  post '/signup',  to: 'users#create'
+  get     '/login',    to: 'sessions#new'
+
+  post    '/login',    to: 'sessions#create'
+
+  delete  '/logout',   to: 'sessions#destroy'
 
   resources :users
   
